6f849ebc4231cc0bcdab493c874d86b5bb4d00f8
Jay Jayabose <jay.jayabose@kin.com>
2025-03-06 22:16:34 -0500

    codecrafters submit [skip ci]

--- app/main.rb
+++ app/main.rb
@@ -1,8 +1,21 @@
 # Uncomment this block to pass the first stage
 require 'pry'
+class Command
+  def self.call
+    raise NotImplementedError
+  end
+end
+
+class Echo < Command
+  def self.call(args) = $stdout.write("#{args.join(' ')} \n")
+end
+
 class Shell
 
   COMMANDS = %w[exit echo]
+  EXECUTORS = {
+    "echo" => Echo
+  }
 
   attr_accessor :command, :args
 
@@ -14,21 +27,24 @@ class Shell
     $stdout.write("$ ")
     
     # Wait for user input
-    self.command, *self.args = gets.chomp.split(" ")
-    validate_command
-    
+    self.command, *self.args = gets.downcase.chomp.split(" ")
+    process_input
   end
 
-  def validate_command
-    return if command.downcase == "exit"
+  def process_input
+    return if command == "exit"
 
-    if command.downcase == "echo"
-      $stdout.write("#{args.join(' ')} \n") 
+    if command_is_valid?(command:)
+      EXECUTORS[command].call(args)
     else
       $stdout.write("#{command}: command not found\n")
     end
 
     get_input
+  end
+
+  def command_is_valid?(command:)
+    COMMANDS.include?(command)
   end
 end
 
